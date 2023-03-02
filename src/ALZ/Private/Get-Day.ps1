61ed920d04899841afcb2d7a396c88338df60749
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2023-03-02 16:18:08 +0000

    Add basic Catesta module skeleton

---
+++ src/ALZ/Private/Get-Day.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    C:\PS>
+    Example of how to use this cmdlet
+.EXAMPLE
+    C:\PS>
+    Another example of how to use this cmdlet
+.PARAMETER InputObject
+    Specifies the object to be processed.  You can also pipe the objects to this command.
+.OUTPUTS
+    Output from this cmdlet (if any)
+.NOTES
+    General notes
+.COMPONENT
+    ALZ
+#>
+function Get-Day {
+    [CmdletBinding()]
+    param (
+        # [Parameter(Mandatory = $true,
+        #     HelpMessage = 'Helpful Message')]
+        # [ValidateNotNull()]
+        # [ValidateNotNullOrEmpty()]
+        # [string]$YourParameter
+    )
+    try {
+        $day = (Get-Date -ErrorAction 'Stop').DayOfWeek
+    }
+    catch {
+        $day = 'Unknown'
+    }
+
+    return $day
+} #Get-Day
+
