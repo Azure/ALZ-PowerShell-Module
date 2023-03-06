64bf119d98c131887dea8525b83277056e2c8a7d
guy.pritchard <guy.pritchard@noreply.com>
2023-03-06 11:55:44 +0000

    Updating test

--- src/Tests/Unit/Public/New-ALZEnvironment.Tests.ps1
+++ src/Tests/Unit/Public/New-ALZEnvironment.Tests.ps1
@@ -28,17 +28,33 @@ InModuleScope 'ALZ' {
 
             BeforeEach {
                 Mock -CommandName Request-ALZEnvironmentConfig -MockWith {
-                    'output/prefix/environment'
+                    @(
+                        @{
+                            "description"  = "Test configuration1"
+                            "names"        = @("value1", "value2")
+                            "defaultValue" = "default"
+                            "value"        = "value"
+                        },
+                        @{
+                            "description"  = "Test configuration2"
+                            "names"        = @("value1")
+                            "defaultValue" = "default"
+                            "value"        = "value"
+                        }
+                    )
                 } #endMock
 
-                Mock -CommandName Edit-ALZConfigurationFilesInPlace -MockWith {
-
-                }
+                Mock -CommandName Edit-ALZConfigurationFilesInPlace
             } #beforeEach
 
             It 'should return the output directory on completion' {
-                New-ALZEnvironment | Should -BeExactly 'output/prefix/environment'
-            } #it
+                $result = New-ALZEnvironment
+                $result[0].description | Should -BeExactly "Test configuration1"
+                $result[0].names[0] | Should -BeExactly "value1"
+                $result[0].names[1] | Should -BeExactly "value2"
+                $result[0].defaultValue | Should -BeExactly "default"
+                $result[0].value | Should -BeExactly "value"
+            }
 
         } #context_Success
     } #describe_Get-HellowWorld
