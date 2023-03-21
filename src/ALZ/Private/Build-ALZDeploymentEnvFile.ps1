c7749e3d3fc495e959f167ebdde7aa86475f53ed
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2023-03-21 04:49:58 +0000

    Correc parameters

--- src/ALZ/Private/Build-ALZDeploymentEnvFile.ps1
+++ src/ALZ/Private/Build-ALZDeploymentEnvFile.ps1
@@ -19,12 +19,12 @@ function Build-ALZDeploymentEnvFile {
 
     $envFile = Join-Path $destination ".env"
 
-    New-Item -Path $envFile -ItemType file -Force | Out-Null
+    New-Item -Path $envFile -ItemType file -Force | Out-String | Write-Verbose
 
     foreach ($configurationValue in $configuration.PsObject.Properties) {
         foreach ($target in $configurationValue.Value.Targets) {
             if ($target.Destination -eq "Environment") {
-                Add-Content -Path $envFile -Value "$($($target.Name))=`"$($configurationValue.Value.Value)`"" | Out-Null
+                Add-Content -Path $envFile -Value "$($($target.Name))=`"$($configurationValue.Value.Value)`"" | Out-String | Write-Verbose
             }
         }
     }
