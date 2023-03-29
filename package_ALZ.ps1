6243939987adfd2610b6d47b6ac9d1f8f2a0154f
Guy Pritchard MSFT <111762521+lovelysandwich@users.noreply.github.com>
2023-03-29 16:03:37 +0100

    Fix broken publishing (#24)
    # Pull Request
    
    ## Description
    
    Changing the release process and pipelines to support a workflow where
    we can publish changes to an alpha channel for testing.
    Proper releases are a manual workflow to support a decision to take a
    release 'live'.
    
    GitHub releases now used as a features.
    
    ## License
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the projects associated license.

--- package_ALZ.ps1
+++ package_ALZ.ps1
@@ -7,3 +7,4 @@ New-Item "ALZ" -ItemType Directory -Force
 Copy-Item -Path "./src/Artifacts/*" -Destination "./ALZ" -Recurse -Exclude "ccReport", "testOutput"  -Force
 
 Update-ModuleManifest -Path "./ALZ/ALZ.psd1" -ModuleVersion $version -Prerelease $prerelease
+
