d2d7a5da4ce4f2194e784d4b5c14b0fcfc22a5c0
Guy Pritchard <guypritchard@microsoft.com>
2023-03-29 11:30:35 +0100

    Adopting CR comments and tidy up

--- package_ALZ.ps1
+++ package_ALZ.ps1
@@ -4,10 +4,6 @@ param(
 )
 
 New-Item "ALZ" -ItemType Directory -Force
-Copy-Item -Path "./src/Artifacts/Assets" -Destination "./ALZ/" -Recurse -Force
-Copy-Item -Path "./src/Artifacts/Private" -Destination "./ALZ/" -Recurse -Force
-Copy-Item -Path "./src/Artifacts/Public" -Destination "./ALZ/" -Recurse -Force
-Copy-Item -Path "./src/Artifacts/ALZ.psd1" -Destination "./ALZ/" -Force
-Copy-Item -Path "./src/Artifacts/ALZ.psm1" -Destination "./ALZ/" -Force
+Copy-Item -Path "./src/Artifacts/*" -Destination "./ALZ" -Recurse -Exclude "ccReport", "testOutput"  -Force
 
 Update-ModuleManifest -Path "./ALZ/ALZ.psd1" -ModuleVersion $version -Prerelease $prerelease
