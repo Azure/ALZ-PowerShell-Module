e893692ece6ccc46dc0246b394120429611e8273
Guy Pritchard MSFT <111762521+lovelysandwich@users.noreply.github.com>
2023-03-29 13:46:20 +0100

    Adding publish pipeline (#21)
    # Pull Request
    This aims to add two actions for main build and non-main branch builds.
    
    They're pretty similar honestly - but the main branch currently
    publishes the ALZ module to a hard coded 'beta' channel and other
    branches will publish to a 'alpha' channel for now.
    
    Moved the builds to Ubuntu machines.
    
    ## License
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the projects associated license.

---
+++ package_ALZ.ps1
@@ -0,0 +1,9 @@
+param(
+    [string]$version,
+    [string]$prerelease = ""
+)
+
+New-Item "ALZ" -ItemType Directory -Force
+Copy-Item -Path "./src/Artifacts/*" -Destination "./ALZ" -Recurse -Exclude "ccReport", "testOutput"  -Force
+
+Update-ModuleManifest -Path "./ALZ/ALZ.psd1" -ModuleVersion $version -Prerelease $prerelease
