4be6f5614f3f546067a35e403bc30a9a37fb0d36
Zach Trocinski <ztrocinski@outlook.com>
2023-04-20 22:13:16 -0500

    Prep for alz bicep release

--- src/ALZ/Private/Get-ALZBicepConfig.ps1
+++ src/ALZ/Private/Get-ALZBicepConfig.ps1
@@ -4,7 +4,7 @@ function Get-ALZBicepConfig {
     #>
     param(
         [Parameter(Mandatory = $false)]
-        [string] $alzBicepVersion = "v0.14.1-pre"
+        [string] $alzBicepVersion = "v0.14.0"
     )
     # import the config from the json file inside assets and tranform it to a powershell object
     $bicepConfig = Get-Content -Path (Join-Path $(Get-ScriptRoot) "../Assets/alz-bicep-config" "$alzBicepVersion.config.json" ) | ConvertFrom-Json
