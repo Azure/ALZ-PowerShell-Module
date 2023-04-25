de28c54cd4fcfaee47906595f1cf7fcb6dbce888
Zach Trocinski <ztrocinski@outlook.com>
2023-04-25 11:27:21 -0500

    Switch to version 14

--- src/ALZ.Settings.ps1
+++ src/ALZ.Settings.ps1
@@ -2,4 +2,4 @@
 [version]$script:requiredPSVersion = '5.1.0'
 
 # specify the supported versions of ALZ-Bicep
-$script:ALZBicepSupportedReleases = @('v0.13.0')
+$script:ALZBicepSupportedReleases = @('v0.14.0')
