36c7fde3d5b84c4d3e3c7ae8aae9626eb6a22c4e
Zach Trocinski <ztrocinski@outlook.com>
2023-06-19 16:23:11 -0500

    Added support for v0.15.0-pre config

--- src/ALZ/Private/Get-ALZBicepConfig.ps1
+++ src/ALZ/Private/Get-ALZBicepConfig.ps1
@@ -4,7 +4,7 @@ function Get-ALZBicepConfig {
     #>
     param(
         [Parameter(Mandatory = $false)]
-        [string] $alzBicepVersion = "v0.14.0"
+        [string] $alzBicepVersion = "v0.15.0-pre"
     )
     # import the config from the json file inside assets and tranform it to a powershell object
     $bicepConfig = Get-Content -Path (Join-Path $(Get-ScriptRoot) "../Assets/alz-bicep-config" "$alzBicepVersion.config.json" ) | ConvertFrom-Json
--- src/ALZ/Public/New-ALZEnvironment.ps1
+++ src/ALZ/Public/New-ALZEnvironment.ps1
@@ -19,7 +19,7 @@ function New-ALZEnvironment {
     .EXAMPLE
     New-ALZEnvironment -alzEnvironmentDestination "."
     .EXAMPLE
-    New-ALZEnvironment -alzEnvironmentDestination "." -alzBicepVersion "v0.14.0"
+    New-ALZEnvironment -alzEnvironmentDestination "." -alzBicepVersion "v0.15.0-pre"
     #>
     [CmdletBinding(SupportsShouldProcess = $true)]
     param (
@@ -30,7 +30,7 @@ function New-ALZEnvironment {
         [string] $alzEnvironmentDestination = ".",
 
         [Parameter(Mandatory = $false)]
-        [string] $alzBicepVersion = "v0.14.0",
+        [string] $alzBicepVersion = "v0.15.0-pre",
 
         [Parameter(Mandatory = $false)]
         [ValidateSet("bicep", "terraform")]
