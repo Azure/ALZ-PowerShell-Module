8ed310dceca4f0e00b17be7edd271ab5a31ba1b4
Jared Holgate <jaredholgate@microsoft.com>
2023-10-03 11:59:55 +0100

    bug: fix switch and os selector (#75)

--- src/ALZ/Private/Get-HCLParserTool.ps1
+++ src/ALZ/Private/Get-HCLParserTool.ps1
@@ -20,8 +20,34 @@ function Get-HCLParserTool {
             $os = "darwin"
         }
 
-        $architecture = $($env:PROCESSOR_ARCHITECTURE).ToLower()
-        $toolFileName = "hcl2json_$($os)_$($architecture)"
+        # Enum values can be seen here: https://learn.microsoft.com/en-us/dotnet/api/system.runtime.interopservices.architecture?view=net-7.0#fields
+        $architecture = ([System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture).ToString().ToLower()
+
+        if($architecture -eq "x64") {
+            $architecture = "amd64"
+        }
+        if($architecture -eq "x86") {
+            $architecture = "386"
+        }
+
+        $osAndArchitecture = "$($os)_$($architecture)"
+
+        $supportedOsAndArchitectures = @(
+            "darwin_amd64",
+            "darwin_arm64",
+            "linux_386",
+            "linux_amd64",
+            "linux_arm64",
+            "windows_386",
+            "windows_amd64"
+        )
+
+        if($supportedOsAndArchitectures -notcontains $osAndArchitecture) {
+            Write-Error "Unsupported OS and architecture combination: $osAndArchitecture"
+            exit 1
+        }
+
+        $toolFileName = "hcl2json_$osAndArchitecture"
 
         if($os -eq "windows") {
             $toolFileName = "$($toolFileName).exe"
@@ -31,6 +57,13 @@ function Get-HCLParserTool {
 
         if(!(Test-Path $toolFilePath)) {
             Invoke-WebRequest -Uri "https://github.com/tmccombs/hcl2json/releases/download/$($toolVersion)/$($toolFileName)" -OutFile "$toolFilePath" | Out-String | Write-Verbose
+        }
+
+        if($os -ne "windows") {
+            $isExecutable = $(test -x $toolFilePath; 0 -eq $LASTEXITCODE)
+            if(!($isExecutable)) {
+                chmod +x $toolFilePath
+            }
         }
     }
 
