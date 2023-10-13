9781c2d3171279561a82e40fbbd723b071fec0cc
Jared Holgate <jaredholgate@microsoft.com>
2023-10-13 16:18:53 +0100

    feature: add yaml support for override config (#82)
    Co-authored-by: Jack Tracey <41163455+jtracey93@users.noreply.github.com>

--- src/ALZ/Private/Get-ALZConfig.ps1
+++ src/ALZ/Private/Get-ALZConfig.ps1
@@ -15,7 +15,19 @@ function Get-ALZConfig {
 
     # Import the config from the json file inside assets and transform it to a PowerShell object
     if ($configFilePath -ne "") {
-        $config = Get-Content -Path $configFilePath | ConvertFrom-Json
+        $extension = (Get-Item -Path $configFilePath).Extension.ToLower()
+        if($extension -eq ".yml" -or $extension -eq ".yaml") {
+            if (!(Get-Module -ListAvailable -Name powershell-Yaml)) {
+                Write-Host "Installing YAML module"
+                Install-Module powershell-Yaml -Force
+            }
+            $config = [PSCustomObject](Get-Content -Path $configFilePath | ConvertFrom-Yaml)
+        } elseif($extension -eq ".json") {
+            $config = Get-Content -Path $configFilePath | ConvertFrom-Json
+        } else {
+            throw "The config file must be a json or yaml/yml file"
+        }
+
         return $config
     }
 
