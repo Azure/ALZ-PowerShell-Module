41a7a1617aa7e905f9efa4222f0a23581a349f2e
Jared Holgate <jaredholgate@microsoft.com>
2024-01-23 00:59:32 +0000

    enhacement: add better error handling and masking for inputs (#101)
    # Pull Request
    
    ## Issue
    
    Issue #, if available: N/A
    
    ## Description
    
    Description of changes:
    
    - Reported by VBD team that the error message is not clear when invalid
    yaml or json are passed.
    
    - The PAT is not masked when using cached values on a second run.
    
    
    ## License
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the projects associated license.

--- src/ALZ/Private/Get-ALZConfig.ps1
+++ src/ALZ/Private/Get-ALZConfig.ps1
@@ -13,14 +13,28 @@ function Get-ALZConfig {
 
     # Import the config and transform it to a PowerShell object
     $extension = (Get-Item -Path $configFilePath).Extension.ToLower()
+    $config = $null
     if($extension -eq ".yml" -or $extension -eq ".yaml") {
         if (!(Get-Module -ListAvailable -Name powershell-Yaml)) {
             Write-Host "Installing YAML module"
             Install-Module powershell-Yaml -Force
         }
-        $config = [PSCustomObject](Get-Content -Path $configFilePath | ConvertFrom-Yaml)
+        try {
+            $config = [PSCustomObject](Get-Content -Path $configFilePath | ConvertFrom-Yaml)
+        } catch {
+            $errorMessage = "Failed to parse YAML inputs. Please check the YAML file for errors and try again. $_"
+            Write-Error $errorMessage
+            throw $errorMessage
+        }
+
     } elseif($extension -eq ".json") {
-        $config = Get-Content -Path $configFilePath | ConvertFrom-Json
+        try {
+            $config = [PSCustomObject](Get-Content -Path $configFilePath | ConvertFrom-Json)
+        } catch {
+            $errorMessage = "Failed to parse JSON inputs. Please check the JSON file for errors and try again. $_"
+            Write-Error $errorMessage
+            throw $errorMessage
+        }
     } else {
         throw "The config file must be a json or yaml/yml file"
     }
