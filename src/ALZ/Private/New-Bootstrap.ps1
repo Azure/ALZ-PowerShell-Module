d38859f4404572b7b8f76478c9c4c2e757112075
Jared Holgate <jaredholgate@microsoft.com>
2024-02-23 08:44:36 -0800

    Save changes

--- src/ALZ/Private/New-Bootstrap.ps1
+++ src/ALZ/Private/New-Bootstrap.ps1
@@ -92,77 +92,77 @@ function New-Bootstrap {
         # Getting the configuration for the interface user input and validators
         $inputConfigMapped = Convert-InterfaceInputToUserInputConfig -inputConfig $inputConfig -validators $validationConfig
         $interfaceConfiguration = Request-ALZEnvironmentConfig -configurationParameters $inputConfigMapped -respectOrdering -userInputOverrides $userInputOverrides -userInputDefaultOverrides $interfaceCachedConfig -treatEmptyDefaultAsValid $true -autoApprove:$autoApprove.IsPresent
+        $starterModuleName = $interfaceConfiguration.PsObject.Properties["starter_module"].Value.Value
+        $starterModulePath = Join-Path -Path $starterPath -ChildPath $starterModuleName
+        $pipelineModulePath = Join-Path -Path $starterPath -ChildPath $bootstrapDetails.Value.pipeline_folder
 
         $computedInputMapping = @{
             "iac_type" = $iac
-            "module_folder_path" = #TBC
-            "pipeline_folder_path" = #TBC
+            "module_folder_path" = $starterModulePath
+            "pipeline_folder_path" = $pipelineModulePath
         }
 
         $inputConfig.PSCustomObject.Properties | ForEach-Object {
             $property = $_
             if($property.Value.source -eq "powershell") {
-                if($property.Name -eq "iac_type") {
-                    $input = $interfaceConfiguration | Where-Object { $_.Name -eq $property.Name }
-                    $input.Value.Value = $iac
-                }
+                $inputVariable = $interfaceConfiguration | Where-Object { $_.Name -eq $property.Name }
+                $inputVariable.Value.Value = $computedInputMapping[$property.Name]
             }
         }
 
-        # Getting additional configuration for the bootstrap module user input
-        $bootstrapVariableFilesPath = Join-Path -Path $bootstrapModulePath -ChildPath "variables.tf"
         $hclParserToolPath = Get-HCLParserTool -alzEnvironmentDestination $bootstrapPath -toolVersion "v0.6.0"
-        $bootstrapParameters = Convert-HCLVariablesToUserInputConfig -targetVariableFile $bootstrapVariableFilesPath -hclParserToolPath $hclParserToolPath -validators $bootstrapConfig.validators
+
+        $bootstrapParameters = [PSCustomObject]@{}
+
+        # Getting additional configuration for the bootstrap module user input
+        foreach($inputVariablesFile in $bootstrapDetails.Value.input_variable_files) {
+            $inputVariablesFilePath = Join-Path -Path $bootstrapModulePath -ChildPath $inputVariablesFile
+            $bootstrapParameters = Convert-HCLVariablesToUserInputConfig -targetVariableFile $inputVariablesFilePath -hclParserToolPath $hclParserToolPath -validators $validationConfig.validators -appendToObject $bootstrapParameters
+        }
 
         Write-InformationColored "Got configuration" -ForegroundColor Green -InformationAction Continue
 
-        # Checking for cached bootstrap values for retry / upgrade scenarios
-        $bootstrapCachedValuesPath = Join-Path -Path $bootstrapPath -ChildPath $cacheFileName
-        $cachedBootstrapConfig = Get-ALZConfig -configFilePath $bootstrapCachedValuesPath
-
         # Getting the user input for the bootstrap module
-        $bootstrapConfiguration = Request-ALZEnvironmentConfig -configurationParameters $bootstrapParameters -respectOrdering -userInputOverrides $userInputOverrides -userInputDefaultOverrides $cachedBootstrapConfig -treatEmptyDefaultAsValid $true -autoApprove:$autoApprove.IsPresent
+        $bootstrapConfiguration = Request-ALZEnvironmentConfig -configurationParameters $bootstrapParameters -respectOrdering -userInputOverrides $userInputOverrides -userInputDefaultOverrides $bootstrapCachedConfig -treatEmptyDefaultAsValid $true -autoApprove:$autoApprove.IsPresent
 
         # Getting the configuration for the starter module user input
-        $starterTemplate = $interfaceConfiguration.PsObject.Properties["starter_module"].Value.Value
-        $starterTemplatePath = Join-Path -Path $starterFolderPath -ChildPath $starterTemplate
-        $targetVariableFilePath = Join-Path -Path $starterTemplatePath -ChildPath "variables.tf"
+        $targetVariableFilePath = Join-Path -Path $starterModulePath -ChildPath "variables.tf"
         $starterModuleParameters = Convert-HCLVariablesToUserInputConfig -targetVariableFile $targetVariableFilePath -hclParserToolPath $hclParserToolPath -validators $bootstrapConfig.validators
 
         Write-InformationColored "The following inputs are specific to the '$starterTemplate' starter module that you selected..." -ForegroundColor Green -InformationAction Continue
 
-        # Checking for cached starter module values for retry / upgrade scenarios
-        $starterModuleCachedValuesPath = Join-Path -Path $starterFolderPath -ChildPath $cacheFileName
-        $cachedStarterModuleConfig = Get-ALZConfig -configFilePath $starterModuleCachedValuesPath
-
         # Getting the user input for the starter module
-        $starterModuleConfiguration = Request-ALZEnvironmentConfig -configurationParameters $starterModuleParameters -respectOrdering -userInputOverrides $userInputOverrides -userInputDefaultOverrides $cachedStarterModuleConfig -treatEmptyDefaultAsValid $true -autoApprove:$autoApprove.IsPresent
+        $starterConfiguration = Request-ALZEnvironmentConfig -configurationParameters $starterModuleParameters -respectOrdering -userInputOverrides $userInputOverrides -userInputDefaultOverrides $starterCachedConfig -treatEmptyDefaultAsValid $true -autoApprove:$autoApprove.IsPresent
 
-        # Getting computed inputs
-        $starterPipelinePath = Join-Path -Path $starterFolderPath -ChildPath $starterPipelineFolder
-
-        Import-StarterPath -bootstrapConfiguration $bootstrapConfiguration -starterPath starterTemplatePath -starterPipelinePath $starterPipelinePath
-        Import-SubscriptionData -starterModuleConfiguration $starterModuleConfiguration -bootstrapConfiguration $bootstrapConfiguration
-        Import-ConfigurationFileData -starterModuleConfiguration $starterModuleConfiguration -bootstrapConfiguration $bootstrapConfiguration
+        # Appending interface configuration to the boostrap and starter configuration
+        foreach($inputConfigItem in $inputConfig.PSObject.Properties) {
+            $inputVariable = $interfaceConfiguration | Where-Object { $_.Name -eq $inputConfigItem.Name }
+            if("bootstrap" -in $inputConfigItem.Value.maps_to) {
+                $bootstrapConfiguration | Add-Member -MemberType NoteProperty -Name $inputVariable.Name -Value $inputVariable.Value
+            }
+            if("starter" -in $inputConfigItem.Value.maps_to) {
+                $starterConfiguration | Add-Member -MemberType NoteProperty -Name $inputVariable.Name -Value $inputVariable.Value
+            }
+        }
 
         # Creating the tfvars files for the bootstrap and starter module
-        $bootstrapTfvarsPath = Join-Path -Path $bootstrapPath -ChildPath "override.tfvars"
-        $starterModuleTfvarsPath = Join-Path -Path $starterTemplatePath -ChildPath "terraform.tfvars"
+        $bootstrapTfvarsPath = Join-Path -Path $bootstrapModulePath -ChildPath "override.tfvars"
+        $starterTfvarsPath = Join-Path -Path $starterModulePath -ChildPath "terraform.tfvars"
         Write-TfvarsFile -tfvarsFilePath $bootstrapTfvarsPath -configuration $bootstrapConfiguration
-        Write-TfvarsFile -tfvarsFilePath $starterModuleTfvarsPath -configuration $starterModuleConfiguration
+        Write-TfvarsFile -tfvarsFilePath $starterTfvarsPath -configuration $starterConfiguration
 
         # Caching the bootstrap and starter module values paths for retry / upgrade scenarios
-        Write-ConfigurationCache -filePath $bootstrapCachedValuesPath -configuration $bootstrapConfiguration
-        Write-ConfigurationCache -filePath $starterModuleCachedValuesPath -configuration $starterModuleConfiguration
+        Write-ConfigurationCache -filePath $bootstrapCachePath -configuration $bootstrapConfiguration
+        Write-ConfigurationCache -filePath $starterCachedPath -configuration $starterConfiguration
 
         # Running terraform init and apply
         Write-InformationColored "Thank you for providing those inputs, we are now initializing and applying Terraform to bootstrap your environment..." -ForegroundColor Green -InformationAction Continue
 
         if($autoApprove) {
-            Invoke-Terraform -moduleFolderPath $bootstrapPath -tfvarsFileName "override.tfvars" -autoApprove -destroy:$destroy.IsPresent
+            Invoke-Terraform -moduleFolderPath $bootstrapModulePath -tfvarsFileName "override.tfvars" -autoApprove -destroy:$destroy.IsPresent
         } else {
             Write-InformationColored "Once the plan is complete you will be prompted to confirm the apply. You must enter 'yes' to apply." -ForegroundColor Green -InformationAction Continue
-            Invoke-Terraform -moduleFolderPath $bootstrapPath -tfvarsFileName "override.tfvars" -destroy:$destroy.IsPresent
+            Invoke-Terraform -moduleFolderPath $bootstrapModulePath -tfvarsFileName "override.tfvars" -destroy:$destroy.IsPresent
         }
     }
 }
