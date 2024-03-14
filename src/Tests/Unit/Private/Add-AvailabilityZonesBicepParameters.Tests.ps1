44e35164ba134a933310e399a8d1ca1e2120e557
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2024-03-14 19:14:09 +0200

    Add availability zones to hub networking parameters

--- src/Tests/Unit/Private/Add-AvailabilityZonesBicepParameters.Tests.ps1
+++ src/Tests/Unit/Private/Add-AvailabilityZonesBicepParameters.Tests.ps1
@@ -13,21 +13,21 @@ Import-Module $PathToManifest -Force
 
 InModuleScope 'ALZ' {
     Describe "Add-AvailabilityZonesBicepParameter" {
-        Context "Adding availability zones to parameters file" {
+        BeforeAll {
+            $alzEnvironmentDestination = "TestDrive:\"
+            $hubParametersPath = "https://raw.githubusercontent.com/Azure/ALZ-Bicep/main/infra-as-code/bicep/modules/hubNetworking/parameters/hubNetworking.parameters.all.json"
+            Invoke-WebRequest -Uri $hubParametersPath -OutFile "$alzEnvironmentDestination\hubNetworking.parameters.all.json"
+        }
+        Context "Hub networking parameters availability zones check" {
             BeforeAll {
-                $alzEnvironmentDestination = "TestDrive:\"
-                $hubParametersPath = "https://raw.githubusercontent.com/Azure/ALZ-Bicep/main/infra-as-code/bicep/modules/hubNetworking/parameters/hubNetworking.parameters.all.json"
-                # Downloading parameters files
-                Invoke-WebRequest -Uri $hubParametersPath -OutFile "$alzEnvironmentDestination\hubNetworking.parameters.all.json"
                 Mock -CommandName Join-Path -MockWith {
                     $alzEnvironmentDestination + "\hubNetworking.parameters.all.json"
                 }
-
                 Mock -CommandName Get-Content -ParameterFilter { $Path -contains 'parametersFilePath' } -MockWith {
                     Get-Content -Path "TestDrive:\hubNetworking.parameters.all.json"
                 }
             }
-            It "Should add the correct zones per region" {
+            It "Should add 3 availability zones for hub networking parameters" {
                 Add-AvailabilityZonesBicepParameter -alzEnvironmentDestination $alzEnvironmentDestination -configFile ([PSCustomObject]@{
                         zonesSupport = @(
                             [PSCustomObject]@{
@@ -36,12 +36,41 @@ InModuleScope 'ALZ' {
                             }
                         )
                     })
-
                 $parametersFileJsonContent = Get-Content -Path "TestDrive:\hubNetworking.parameters.all.json" -Raw
                 $jsonObject = $parametersFileJsonContent | ConvertFrom-Json
                 $jsonObject.parameters.parAzErGatewayAvailabilityZones.value | Should -Be @("1", "2", "3")
                 $jsonObject.parameters.parAzVpnGatewayAvailabilityZones.value | Should -Be @("1", "2", "3")
                 $jsonObject.parameters.parAzFirewallAvailabilityZones.value | Should -Be @("1", "2", "3")
+            }
+            It "Should add 2 availability zones for hub networking parameters" {
+                Add-AvailabilityZonesBicepParameter -alzEnvironmentDestination $alzEnvironmentDestination -configFile ([PSCustomObject]@{
+                        zonesSupport = @(
+                            [PSCustomObject]@{
+                                region = "eastus"
+                                zones  = @("1", "2")
+                            }
+                        )
+                    })
+                $parametersFileJsonContent = Get-Content -Path "TestDrive:\hubNetworking.parameters.all.json" -Raw
+                $jsonObject = $parametersFileJsonContent | ConvertFrom-Json
+                $jsonObject.parameters.parAzErGatewayAvailabilityZones.value | Should -Be @("1", "2")
+                $jsonObject.parameters.parAzVpnGatewayAvailabilityZones.value | Should -Be @("1", "2")
+                $jsonObject.parameters.parAzFirewallAvailabilityZones.value | Should -Be @("1", "2")
+            }
+            It "Should add 0 availability zones for hub networking parameters" {
+                Add-AvailabilityZonesBicepParameter -alzEnvironmentDestination $alzEnvironmentDestination -configFile ([PSCustomObject]@{
+                        zonesSupport = @(
+                            [PSCustomObject]@{
+                                region = "eastus"
+                                zones  = @()
+                            }
+                        )
+                    })
+                $parametersFileJsonContent = Get-Content -Path "TestDrive:\hubNetworking.parameters.all.json" -Raw
+                $jsonObject = $parametersFileJsonContent | ConvertFrom-Json
+                $jsonObject.parameters.parAzErGatewayAvailabilityZones.value | Should -Be @()
+                $jsonObject.parameters.parAzVpnGatewayAvailabilityZones.value | Should -Be @()
+                $jsonObject.parameters.parAzFirewallAvailabilityZones.value | Should -Be @()
             }
         }
     }
