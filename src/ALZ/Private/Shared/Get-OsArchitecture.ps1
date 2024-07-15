57941e2ec966208275a7dfe418809886e2ec8da4
Jared Holgate <jaredholgate@microsoft.com>
2024-07-15 14:46:33 +0100

    Support for Windows arm64

--- src/ALZ/Private/Shared/Get-OsArchitecture.ps1
+++ src/ALZ/Private/Shared/Get-OsArchitecture.ps1
@@ -38,6 +38,12 @@ function Get-OSArchitecture {
         exit 1
     }
 
+    if($osAndArchitecture -eq "windows_arm64") {
+        Write-InformationColoured "Windows arm64 is not currently supported by Terraform, so we will pull the Windows amd64 verison instead and run in emulation mode: https://learn.microsoft.com/en-us/windows/arm/apps-on-arm-x86-emulation" -ForegroundColor Yellow -NewLineBefore -InformationAction Continue
+        $architecture = "amd64"
+        $osAndArchitecture = "windows_amd64"
+    }
+
     return @{
         os                = $os
         architecture      = $architecture
