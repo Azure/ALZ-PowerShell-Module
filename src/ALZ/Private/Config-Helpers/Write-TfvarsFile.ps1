dbef58c835fe87c5e8447b0be4ad55949d8c7455
Zach Trocinski <ztrocinski@outlook.com>
2024-08-22 20:44:09 -0500

    Initial commit with working map(string) check

--- src/ALZ/Private/Config-Helpers/Write-TfvarsFile.ps1
+++ src/ALZ/Private/Config-Helpers/Write-TfvarsFile.ps1
@@ -23,17 +23,61 @@ function Write-TfvarsFile {
 
             $configurationValue = "`"$($configurationValueRaw)`""
 
-            if($configurationProperty.Value.DataType -eq "list(string)") {
-                if($configurationValueRaw -eq "") {
+            if ($configurationProperty.Value.DataType -eq "list(string)") {
+                if ($configurationValueRaw -eq "") {
                     $configurationValue = "[]"
                 } else {
                     $split = $configurationValueRaw -split ","
                     $join = $split -join "`",`""
                     $configurationValue = "[`"$join`"]"
                 }
+                Write-Host "list(string) - Raw Value: $configurationValueRaw"
             }
 
-            if($configurationProperty.Value.DataType -eq "number" -or $configurationProperty.Value.DataType -eq "bool") {
+            if ($configurationProperty.Value.DataType -eq "map(string)") {
+                if (-not $configurationValueRaw -or $configurationValueRaw.Count -eq 0) {
+                    $configurationValue = "{}"
+                } else {
+                    $configurationValue = "{"
+                    $entries = @()
+
+                    foreach ($key in $configurationValueRaw.Keys) {
+                        $value = $configurationValueRaw[$key]
+                        $entries += "`"$key`": `"$value`""
+                    }
+
+                    $configurationValue = $entries -join ", "
+                    $configurationValue = "{ $configurationValue }"
+                }
+                Write-Host "map(string) - Processed Value: $configurationValue"
+            }
+
+            if ($configurationProperty.Value.DataType -eq "list(object)") {
+                if ($configurationValueRaw -eq "") {
+                    $configurationValue = "[]"
+                } elseif ($configurationValueRaw -eq "[]") {
+                    $configurationValue = "[]"
+                } else {
+                    $configurationValue = "["
+                    foreach ($entry in $configurationValueRaw) {
+                        $configurationValue += "{ "
+                        foreach ($keyValue in $entry.PSObject.Properties) {
+                            $key = $keyValue.Name
+                            $value = $keyValue.Value
+                            $configurationValue += "`"$key`": `"$value`", "
+                        }
+                        $configurationValue = $configurationValue.TrimEnd(", ")
+                        $configurationValue += "}, "
+                    }
+                    $configurationValue = $configurationValue.TrimEnd(", ")
+                    $configurationValue += "]"
+                }
+                Write-Host "list(object) - Raw Value: $configurationValueRaw"
+                Write-Host "list(object) - Processed Value: $configurationValue"
+            }
+
+
+            if ($configurationProperty.Value.DataType -eq "number" -or $configurationProperty.Value.DataType -eq "bool") {
                 $configurationValue = $configurationValueRaw
             } else {
                 $configurationValue = $configurationValue.Replace("\", "\\")
