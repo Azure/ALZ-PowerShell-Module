bf807a869e5c08ebf9f265b40f5d70f191544b83
Jared Holgate <jaredholgate@microsoft.com>
2025-01-21 11:51:32 +0000

    feat: requirements (#279)
    # Pull Request
    
    ## Description
    
    Update the requirements and make it mandatory
    
    This PR also includes some clean up and simplification of redundant
    code.
    
    Flagging as a minor version, but there are no known breaking changes.
    
    Example with failure:
    <img width="965" alt="image"
    src="https://github.com/user-attachments/assets/9f0b1581-ad6d-4af9-ad51-2c30ec175c39"
    />
    
    Example without failure:
    <img width="880" alt="image"
    src="https://github.com/user-attachments/assets/c5c87a15-45b7-4895-93df-9f00498c8a44"
    />
    
    
    ## License
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the projects associated license.

--- src/ALZ/ALZ.psd1
+++ src/ALZ/ALZ.psd1
@@ -72,9 +72,8 @@
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
     FunctionsToExport = @(
-        'New-ALZEnvironment'
-        'Test-ALZRequirement'
-        'Edit-LineEnding'
+        'Test-AcceleratorRequirement',
+        'Deploy-Accelerator'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
@@ -84,10 +83,7 @@
     VariablesToExport = '*'
 
     # Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
-    AliasesToExport   = @(
-        'Edit-LineEndings'
-        'Deploy-Accelerator'
-    )
+    AliasesToExport   = @()
 
     # DSC resources to export from this module
     # DscResourcesToExport = @()
@@ -142,7 +138,4 @@
 
     # Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
     # DefaultCommandPrefix = ''
-
 }
-
-
---
+++ src/ALZ/Private/Tools/Test-Tooling.ps1
@@ -0,0 +1,111 @@
+function Test-Tooling {
+    $checkResults = @()
+    $hasFailure = $false
+
+    # Check if PowerShell is the correct version
+    Write-Verbose "Checking PowerShell version"
+    $powerShellVersionTable = $PSVersionTable
+    $powerShellVersion = $powerShellVersionTable.PSVersion.ToString()
+    if ($powerShellVersionTable.PSVersion.Major -lt 7) {
+        $checkResults += @{
+            message = "PowerShell version $powerShellVersion is not supported. Please upgrade to PowerShell 7.4 or higher. Either switch to the `pwsh` prompt or follow the instructions here: https://aka.ms/install-powershell"
+            result  = "Failure"
+        }
+        $hasFailure = $true
+    } elseif ($powerShellVersionTable.PSVersion.Major -eq 7 -and $powerShellVersionTable.PSVersion.Minor -lt 4) {
+        $checkResults += @{
+            message = "PowerShell version $powerShellVersion is not supported. Please upgrade to PowerShell 7.4 or higher. Either switch to the `pwsh` prompt or follow the instructions here: https://aka.ms/install-powershell"
+            result  = "Failure"
+        }
+        $hasFailure = $true
+    } else {
+        $checkResults += @{
+            message = "PowerShell version $powerShellVersion is supported."
+            result  = "Success"
+        }
+    }
+
+    # Check if Git is installed
+    Write-Verbose "Checking Git installation"
+    $gitPath = Get-Command git -ErrorAction SilentlyContinue
+    if ($gitPath) {
+        $checkResults += @{
+            message = "Git is installed."
+            result  = "Success"
+        }
+    } else {
+        $checkResults += @{
+            message = "Git is not installed. Follow the instructions here: https://git-scm.com/downloads"
+            result  = "Failure"
+        }
+        $hasFailure = $true
+    }
+
+    # Check if Azure CLI is installed
+    Write-Verbose "Checking Azure CLI installation"
+    $azCliPath = Get-Command az -ErrorAction SilentlyContinue
+    if ($azCliPath) {
+        $checkResults += @{
+            message = "Azure CLI is installed."
+            result  = "Success"
+        }
+    } else {
+        $checkResults += @{
+            message = "Azure CLI is not installed. Follow the instructions here: https://learn.microsoft.com/en-us/cli/azure/install-azure-cli"
+            result  = "Failure"
+        }
+        $hasFailure = $true
+    }
+
+    # Check if Azure CLI is logged in
+    Write-Verbose "Checking Azure CLI login status"
+    $azCliAccount = $(az account show -o json) | ConvertFrom-Json
+    if ($azCliAccount) {
+        $checkResults += @{
+            message = "Azure CLI is logged in. Tenant ID: $($azCliAccount.tenantId), Subscription: $($azCliAccount.name) ($($azCliAccount.id))"
+            result  = "Success"
+        }
+    } else {
+        $checkResults += @{
+            message = "Azure CLI is not logged in. Please login to Azure CLI using 'az login -t `"00000000-0000-0000-0000-000000000000}`"', replacing the empty GUID with your tenant ID."
+            result  = "Failure"
+        }
+        $hasFailure = $true
+    }
+
+    # Check if latest ALZ module is installed
+    Write-Verbose "Checking ALZ module version"
+    $alzModuleCurrentVersion = Get-InstalledModule -Name ALZ
+    $alzModuleLatestVersion = Find-Module -Name ALZ
+    if ($alzModuleCurrentVersion.Version -lt $alzModuleLatestVersion.Version) {
+        $checkResults += @{
+            message = "ALZ module is not the latest version. Your version: $($alzModuleCurrentVersion.Version), Latest version: $($alzModuleLatestVersion.Version). Please update to the latest version using 'Update-Module ALZ'."
+            result  = "Failure"
+        }
+        $hasFailure = $true
+    } else {
+        $checkResults += @{
+            message = "ALZ module is the latest version ($($alzModuleCurrentVersion.Version))."
+            result  = "Success"
+        }
+    }
+
+    Write-Verbose "Showing check results"
+    $checkResults | ForEach-Object {[PSCustomObject]$_} | Format-Table -Property @{
+        Label = "Check Result"; Expression = {
+            switch ($_.result) {
+                'Success' { $color = "92"; break }
+                'Failure' { $color = "91"; break }
+                default { $color = "0" }
+            }
+            $e = [char]27
+            "$e[${color}m$($_.result)${e}[0m"
+        }
+    }, @{ Label = "Check Details"; Expression = {$_.message} }  -AutoSize -Wrap
+
+    if($hasFailure) {
+        Write-InformationColored "Accelerator software requirements have no been met, please review and install the missing software." -ForegroundColor Red -InformationAction Continue
+        Write-InformationColored "Cannot continue with Deployment..." -ForegroundColor Red -InformationAction Continue
+        throw "Accelerator software requirements have no been met, please review and install the missing software."
+    }
+}
